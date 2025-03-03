e2be348235364913534c9dab64f34cbb87c5557b
Yuriy Sydor <yuriysydor1991@gmail.com>
2025-03-03 20:04:40 +0200

    Adding main application function explanatory comment.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,13 @@
 #include "src/app/ApplicationFactory.h"
 
-int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv)
+/**
+ * @brief The application main routine, obviously.
+ * 
+ * @param argc Count of given command line arguments.
+ * @param argv The command line parameters array by itself.
+ * 
+ * @return Returns a zero value in case of success and other value 
+ * in case of any value.
+ */
+int main(int argc, char** argv)
 { return app::ApplicationFactory::execute (argc, argv); }

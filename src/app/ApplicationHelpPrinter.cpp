30aead483cd9898ef8501283ba564a4593f6762c
Yuriy Sydor <yuriysydor1991@gmail.com>
2025-03-06 10:45:28 +0200

    Introducing comprehensive help message in the ApplicationHelpPrinter

--- src/app/ApplicationHelpPrinter.cpp
+++ src/app/ApplicationHelpPrinter.cpp
@@ -3,6 +3,7 @@
 #include <cassert>
 
 #include "src/app/ApplicationHelpPrinter.h"
+#include "project-global-decls.h"
 
 namespace app {
 
@@ -14,7 +15,18 @@ int ApplicationHelpPrinter::run(std::shared_ptr<ApplicationContext> ctx)
   { return INVALID ; }
 
   // Register and implement here command line parameters from the CommandLineParser class.
-  std::cout << "Application help message here!" << std::endl;
+  std::cout
+    << "Usage:" << std::endl << std::endl
+    << "\t" << project_decls::PROJECT_NAME << " [OPTIONS]" 
+    << std::endl << std::endl
+    << "Introduce a new command line flags by registering them in" << std::endl
+    << "the ApplicationHelpPrinter and the CommandLineParser classes."
+    << std::endl << std::endl
+    << "Where OPTIONS may be next:" << std::endl
+    << "\t--help or -h - print current help message"
+    << std::endl
+    << "\t--version or -v - print application version, build git commit and configure date"
+    << std::endl;
 
   return 0 ;
 }

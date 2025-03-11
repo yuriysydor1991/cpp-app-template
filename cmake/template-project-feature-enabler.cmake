37b2e4827e1487b3badabb7605f03efadc8a92d2
Yuriy Sydor <yuriysydor1991@gmail.com>
2025-03-11 10:33:24 +0200

    Decomposing feature enabling if statements into separate file

---
+++ cmake/template-project-feature-enabler.cmake
@@ -0,0 +1,14 @@
+cmake_minimum_required(VERSION 3.13)
+
+if(ENABLE_UNIT_TESTS)
+  enable_testing()
+  include(template-project-make-GTest-available)
+endif()
+
+if(ENABLE_CLANGFORMAT)
+  include(template-project-clang-format-target)
+endif()
+
+if(ENABLE_CPPCHECK)
+  include(template-project-cppcheck-target)
+endif()

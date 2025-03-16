045a1a45df151e2b37927bd545ea55ed95d44656
Yuriy Sydor <yuriysydor1991@gmail.com>
2025-03-16 14:43:22 +0200

    Refining the Application UTs.

---
+++ src/app/tests/mocks/GtkmmIniter/src/gtkmm/GtkmmIniter.h
@@ -0,0 +1,30 @@
+#ifndef YOUR_CPP_APP_TEMPLATE_PROJECT_GTKMMINITER_CLASS_H
+#define YOUR_CPP_APP_TEMPLATE_PROJECT_GTKMMINITER_CLASS_H
+
+#include <gmock/gmock.h>
+
+#include <functional>
+#include <memory>
+
+namespace templateGtkmm
+{
+
+class GtkmmIniter
+{
+ public:
+  virtual ~GtkmmIniter() = default;
+  GtkmmIniter()
+  {
+    if (onMockCreate) {
+      onMockCreate(*this);
+    }
+  }
+
+  inline static std::function<void(GtkmmIniter&)> onMockCreate;
+
+  MOCK_METHOD(int, run, (int& argc, char**& argv));
+};
+
+}  // namespace templateGtkmm
+
+#endif  // YOUR_CPP_APP_TEMPLATE_PROJECT_GTKMMINITER_CLASS_H
--- src/app/tests/unit/Application/UTEST_Application.cpp
+++ src/app/tests/unit/Application/UTEST_Application.cpp
@@ -2,8 +2,10 @@
 #include <gtest/gtest.h>
 
 #include "src/app/Application.h"
+#include "src/gtkmm/GtkmmIniter.h"
 
 using namespace app;
+using namespace templateGtkmm;
 using namespace testing;
 
 class UTEST_Application : public Test
@@ -14,6 +16,8 @@ class UTEST_Application : public Test
         appCtx{std::make_shared<ApplicationContext>(argc, argv)}
   {
   }
+
+  ~UTEST_Application() { GtkmmIniter::onMockCreate = nullptr; }
 
   int argc{0};
   char** argv{nullptr};
@@ -26,9 +30,45 @@ TEST_F(UTEST_Application, no_context_error) { EXPECT_NE(app->run({}), 0); }
 
 TEST_F(UTEST_Application, normal_exit)
 {
+  MockFunction<void(GtkmmIniter & instance)> initerEnsurer;
+
+  EXPECT_CALL(initerEnsurer, Call(_))
+      .Times(1)
+      .WillOnce(Invoke([&](GtkmmIniter& instance) {
+        EXPECT_CALL(instance, run(appCtx->argc, appCtx->argv))
+            .Times(1)
+            .WillOnce(Return(0));
+      }));
+
+  GtkmmIniter::onMockCreate = initerEnsurer.AsStdFunction();
+
   EXPECT_CALL(*appCtx, push_error(_)).Times(0);
 
   EXPECT_EQ(app->run(appCtx), 0);
+
+  EXPECT_TRUE(appCtx->errors.empty());
+
+  EXPECT_FALSE(appCtx->print_help_and_exit);
+  EXPECT_FALSE(appCtx->print_version_and_exit);
+}
+
+TEST_F(UTEST_Application, invalid_exit_status)
+{
+  MockFunction<void(GtkmmIniter & instance)> initerEnsurer;
+
+  EXPECT_CALL(initerEnsurer, Call(_))
+      .Times(1)
+      .WillOnce(Invoke([&](GtkmmIniter& instance) {
+        EXPECT_CALL(instance, run(appCtx->argc, appCtx->argv))
+            .Times(1)
+            .WillOnce(Return(1));
+      }));
+
+  GtkmmIniter::onMockCreate = initerEnsurer.AsStdFunction();
+
+  EXPECT_CALL(*appCtx, push_error(_)).Times(0);
+
+  EXPECT_NE(app->run(appCtx), 0);
 
   EXPECT_TRUE(appCtx->errors.empty());
 

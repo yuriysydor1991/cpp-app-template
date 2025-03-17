4f00514a615f7123cacc873bf00d59ae86accf7e
Yuriy Sydor <yuriysydor1991@gmail.com>
2025-03-17 17:28:52 +0200

    Introducing MSVC extra compile options for warnings

--- cmake/template-project-compile-options.cmake
+++ cmake/template-project-compile-options.cmake
@@ -1,41 +1,11 @@
 cmake_minimum_required(VERSION 3.13)
 
-# -D_FORTIFY_SOURCE=2 (needs OPT); 
-
-set(EXTRA_COMPILE_OPTIONS 
-  -Wall
-  -Wextra
-  -Wpedantic
-  -Wshadow
-  -Wnon-virtual-dtor
-  -Wold-style-cast
-  -Wcast-align
-  -Wunused
-  -Woverloaded-virtual
-  -Wconversion
-  -Wsign-conversion
-  -Wnull-dereference
-  -Wdouble-promotion
-  -Wformat=2
-  -Wimplicit-fallthrough
-  -Wduplicated-cond
-  -Wduplicated-branches
-  -Wlogical-op
-  -Wuseless-cast
-  -Wmisleading-indentation
-  -Wzero-as-null-pointer-constant
-  -fstack-protector-all
-  -fno-common
-  -fstrict-overflow
-  -pie
-  -Wl,-z,relro
-  -Wl,-z,now
-)
-
-if (COMPILE_WARNINGS_AS_ERRORS)
-  set(EXTRA_COMPILE_OPTIONS ${EXTRA_COMPILE_OPTIONS} -Werror)
+# CMAKE_C_COMPILER_ID STREQUAL "Clang"
+if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+  include(template-project-compile-options-MSVC)
+else()
+  include(template-project-compile-options-GCC)
 endif()
 
-if (ENABLE_COMPILER_CODE_ANALYZER)
-  set(EXTRA_COMPILE_OPTIONS ${EXTRA_COMPILE_OPTIONS} -fanalyzer)
-endif()
+message(STATUS "COMPILER ID: ${CMAKE_CXX_COMPILER_ID}")
+message(STATUS "COMPILE OPTIONS: ${EXTRA_COMPILE_OPTIONS}")

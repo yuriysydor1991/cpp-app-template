d124c7618dddd736b79938cd53c8035aca8b576c
Yuriy Sydor <yuriysydor1991@gmail.com>
2025-05-27 10:56:55 +0300

    Refining the flatpak target final

--- cmake/template-project-MySQLCppConn.cmake
+++
@@ -1,7 +0,0 @@
-cmake_minimum_required(VERSION 3.13)
-
-set(MySQL_INCLUDE_DIRS "/usr/include/cppconn" CACHE STRING "The MySQLCppConn includes path")
-set(MySQL_LIBRARIES "mysqlcppconn" CACHE STRING "The MySQLCppConn libraries")
-
-# doesn't work
-find_package(MySQL COMPONENTS ConnectorCpp)

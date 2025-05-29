48859d84b683989accbd52e76cfe7aa5f00fec1e
Yuriy Sydor <yuriysydor1991@gmail.com>
2025-05-29 14:32:29 +0300

    Making the flatpak source json conf file configurable during cmake conf stage

--- cmake/template-project-flatpak-target.cmake
+++ cmake/template-project-flatpak-target.cmake
@@ -7,14 +7,17 @@ set(FLATPAK_PROJECT_URL ua.org.kytok.template.${PROJECT_BINARY_NAME}.SDL2)
 set(FLATPAK_REPO ${PROJECT_BINARY_NAME}-repo)
 set(FLATPAK_DST_NAME ${PROJECT_BINARY_NAME}-${CMAKE_PROJECT_VERSION}.flatpak)
 set(flatpakConfDst ${CMAKE_BINARY_DIR}/flatpak.conf.json)
-
-configure_file(
-  ${CMAKE_SOURCE_DIR}/misc/flatpak.conf.json.in
-  ${flatpakConfDst}
+set(
+  FLATPAK_CONF_SRC 
+  ${CMAKE_SOURCE_DIR}/misc/flatpak.conf.json.in 
+  CACHE STRING "The flatpak json conf source file cmake configured"
 )
+
+configure_file(${FLATPAK_CONF_SRC} ${flatpakConfDst})
 
 message(STATUS "flatpak-builder: ${FLATPAKB_EXEC}")
 message(STATUS "flatpak: ${FLATPAK_EXEC}")
+message(STATUS "flatpak json conf src: ${FLATPAK_CONF_SRC}")
 
 add_custom_target(
   flatpak

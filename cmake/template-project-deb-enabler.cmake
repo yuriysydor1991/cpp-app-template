2b1aac5bc7db8f8695e35cae77ad51476b8ff4a7
Yuriy Sydor <yuriysydor1991@gmail.com>
2025-05-29 12:45:18 +0300

    Introducing the Qt6 dependencies into the dep-variables for cpack

--- cmake/template-project-deb-enabler.cmake
+++ cmake/template-project-deb-enabler.cmake
@@ -11,6 +11,11 @@ set(CPACK_DEBIAN_PACKAGE_MAINTAINER "${PROJECT_MAINTAINER} ${PROJECT_MAINTAINER_
 set(CPACK_DEB_COMPONENT_INSTALL ON)
 set(CPACK_PACKAGE_DESCRIPTION ${CMAKE_PROJECT_DESCRIPTION})
 set(CPACK_DEB_PACKAGE_ARCHITECTURE "amd64")  # Or arm64
-set(CPACK_DEBIAN_PACKAGE_DEPENDS "")
+set(CPACK_DEBIAN_PACKAGE_DEPENDS
+    "libqt6core6, libqt6gui6, libqt6widgets6, libqt6qml6, libqt6quick6, 
+    libqt6quickcontrols2-6, libqt6quicktemplates2-6, libqt6qmlworkerscript6, 
+    qml6-module-qtquick, qml6-module-qtqml-workerscript, 
+    qml6-module-qtquick-templates, qml6-module-qtquick-window"
+)
 
 include(CPack)

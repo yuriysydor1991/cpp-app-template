c2bc499b589aa702ebf98dcf38ce12aabe9c2247
Yuriy Sydor <yuriysydor1991@gmail.com>
2025-05-29 12:48:32 +0300

    Introducing the SDL2 dependencies into the deb variables for the cpack

--- cmake/template-project-deb-enabler.cmake
+++ cmake/template-project-deb-enabler.cmake
@@ -11,6 +11,10 @@ set(CPACK_DEBIAN_PACKAGE_MAINTAINER "${PROJECT_MAINTAINER} ${PROJECT_MAINTAINER_
 set(CPACK_DEB_COMPONENT_INSTALL ON)
 set(CPACK_PACKAGE_DESCRIPTION ${CMAKE_PROJECT_DESCRIPTION})
 set(CPACK_DEB_PACKAGE_ARCHITECTURE "amd64")  # Or arm64
-set(CPACK_DEBIAN_PACKAGE_DEPENDS "")
+set(CPACK_DEBIAN_PACKAGE_DEPENDS 
+  "libsdl2-dev, libsdl2-doc,
+  libsdl2-image-2.0-0, libsdl2-image-dev, libsdl2-mixer-2.0-0,
+  libsdl2-mixer-dev, libsdl2-net-2.0-0, libsdl2-net-dev,
+  libsdl2-ttf-dev")
 
 include(CPack)

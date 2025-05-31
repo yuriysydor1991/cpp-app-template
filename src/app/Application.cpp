18fe6c2410c49b8763cbcff308618418de1a1c3c
Yuriy Sydor <yuriysydor1991@gmail.com>
2025-05-31 18:33:40 +0300

    Implementing the logging macros and a simple logger to perform log messaging

--- src/app/Application.cpp
+++ src/app/Application.cpp
@@ -5,6 +5,7 @@
 #include <memory>
 
 #include "src/beasthttp/HttpController.h"
+#include "src/log/log.h"
 
 namespace app
 {
@@ -14,12 +15,14 @@ int Application::run(std::shared_ptr<ApplicationContext> ctx)
   assert(ctx != nullptr);
 
   if (ctx == nullptr) {
+    LOGE("No valid context pointer provided");
     return INVALID;
   }
 
   auto http = std::make_shared<beasthttp::HttpController>();
 
   if (!http->serve(ctx)) {
+    LOGE("The HTTP server returned an invalid status");
     return INVALID;
   }
 

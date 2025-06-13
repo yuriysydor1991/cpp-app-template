dc9ca3c78cd9cb0a474a573514d5d3027526df3a
Yuriy Sydor <yuriysydor1991@gmail.com>
2025-06-13 11:10:26 +0300

    Introducing the CMake configurable log severity messages compilation switch

--- cmake/template-project-misc-variables-declare.cmake
+++ cmake/template-project-misc-variables-declare.cmake
@@ -130,6 +130,12 @@ option(
   OFF
 )
 
+set(
+  MAX_LOG_LEVEL "3"
+  CACHE STRING 
+  "Sets the maximum severity of the compiled log messages. Error=0. Trace=5. By default Info=3"
+)
+
 string(TIMESTAMP PROJECT_CONFIGURE_DATE "%Y-%m-%d %H:%M:%S")
 
 set(PROJECT_HTTP_LISTEN_ADDRESS "127.0.0.1" CACHE STRING "The default HTTP-server listen address")

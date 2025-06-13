8de6aebe961ad3a5c39c3af075bdbdc139bf52d2
Yuriy Sydor <yuriysydor1991@gmail.com>
2025-06-13 11:10:26 +0300

    Introducing the CMake configurable log severity messages compilation switch

--- src/log/log.h
+++ src/log/log.h
@@ -3,16 +3,23 @@
 
 #include <sstream>
 
+#include "src/log/severity-macro-consts.h"
 #include "src/log/simple-logger/SimpleLogger.h"
 
+#ifndef LOG_INIT
 /**
  * @brief The logging init macros. Use them in the main function or
  * as by default is used in the app::ApplicationFactory::execute method.
  */
 #define LOG_INIT(filepath, logLvl, printMessages) \
   simple_logger::SimpleLogger::init(filepath, logLvl, printMessages);
-#define LOG_INIT_DEFAULTS() simple_logger::SimpleLogger::init();
+#endif  // LOG_INIT
 
+#ifndef LOG_INIT_DEFAULTS
+#define LOG_INIT_DEFAULTS() simple_logger::SimpleLogger::init();
+#endif  // LOG_INIT_DEFAULTS
+
+#ifndef LOG_BODY
 /**
  * @brief The internal logger macro to define the general logging code body.
  */
@@ -23,7 +30,9 @@
     simple_logger::SimpleLogger::log(LOGLVL, __FILE__, __LINE__, \
                                      logMessageContainer.str()); \
   }
+#endif  // LOG_BODY
 
+#ifndef LOGE
 /**
  * @brief Perform the error logging.
  *
@@ -31,7 +40,10 @@
  * and each of the log elements MUST be converted into the std::string.
  */
 #define LOGE(msg) LOG_BODY(simple_logger::SimpleLogger::LVL_ERROR, msg)
+#endif  // LOGE
 
+#ifndef LOGI
+#if MAX_LOG_LEVEL >= MACRO_LOG_LEVEL_INFO
 /**
  * @brief Perform the info logging.
  *
@@ -39,7 +51,13 @@
  * and each of the log elements MUST be converted into the std::string.
  */
 #define LOGI(msg) LOG_BODY(simple_logger::SimpleLogger::LVL_INFO, msg)
+#else
+#define LOGI(msg)
+#endif  // MAX_LOG_LEVEL
+#endif  // LOGI
 
+#ifndef LOGW
+#if MAX_LOG_LEVEL >= MACRO_LOG_LEVEL_WARNING
 /**
  * @brief Perform the warning logging.
  *
@@ -47,7 +65,13 @@
  * and each of the log elements MUST be converted into the std::string.
  */
 #define LOGW(msg) LOG_BODY(simple_logger::SimpleLogger::LVL_WARNING, msg)
+#else
+#define LOGW(msg)
+#endif  // MAX_LOG_LEVEL
+#endif  // LOGW
 
+#ifndef LOGD
+#if MAX_LOG_LEVEL >= MACRO_LOG_LEVEL_DEBUG
 /**
  * @brief Perform the debug logging.
  *
@@ -55,7 +79,13 @@
  * and each of the log elements MUST be converted into the std::string.
  */
 #define LOGD(msg) LOG_BODY(simple_logger::SimpleLogger::LVL_DEBUG, msg)
+#else
+#define LOGD(msg)
+#endif  // MAX_LOG_LEVEL
+#endif  // LOGD
 
+#ifndef LOGT
+#if MAX_LOG_LEVEL >= MACRO_LOG_LEVEL_TRACE
 /**
  * @brief Perform the trace logging.
  *
@@ -63,5 +93,9 @@
  * and each of the log elements MUST be converted into the std::string.
  */
 #define LOGT(msg) LOG_BODY(simple_logger::SimpleLogger::LVL_TRACE, msg)
+#else
+#define LOGT(msg)
+#endif  // MAX_LOG_LEVEL
+#endif  // LOGT
 
 #endif  // YOUR_CPP_APP_TEMPLATE_PROJECT_LOGGER_SUBSYSTEM_DECLARATIONS_H

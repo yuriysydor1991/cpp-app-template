2935bfec3a77ea415e7469f59d6e4392544d0ace
Yuriy Sydor <yuriysydor1991@gmail.com>
2025-07-27 19:00:05 +0300

    Introducing the valgrind callgrind profiler target for the project

--- cmake/template-project-misc-variables-declare.cmake
+++ cmake/template-project-misc-variables-declare.cmake
@@ -160,6 +160,12 @@ option(
   OFF
 )
 
+option(
+  ENABLE_CALLGRIND
+  "Set to ON to enable the valgrind (callgrind) application profiler analysis"
+  OFF
+)
+
 string(TIMESTAMP PROJECT_CONFIGURE_DATE "%Y-%m-%d %H:%M:%S")
 
 set(PROJECT_HTTP_LISTEN_ADDRESS "127.0.0.1" CACHE STRING "The default HTTP-server listen address")

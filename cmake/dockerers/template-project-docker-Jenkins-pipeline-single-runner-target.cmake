6ac3d4fc0b74d7a4bb61d44b940700b70e88f120
Yuriy Sydor <yuriysydor1991@gmail.com>
2025-09-16 12:03:23 +0300

    Refining Jenkins pipeline target

--- cmake/dockerers/template-project-docker-Jenkins-pipeline-single-runner-target.cmake
+++ cmake/dockerers/template-project-docker-Jenkins-pipeline-single-runner-target.cmake
@@ -4,6 +4,15 @@ if (NOT ENABLE_JENKINS_DOCKER_PIPELINE)
   return()
 endif()
 
+find_program(SCRIPT_EXEC script REQUIRED)
+
+set(
+  JENKINS_PIPELINES_PANEL_HTTP_PORT
+  "8080"
+  CACHE STRING
+  "The Jenkins UI port on the docker container"
+)
+
 set(
   JENKINS_PIPELINE_DOCKERFILE_SRC 
   "${CMAKE_SOURCE_DIR}/misc/Dockerfile.Jenkins.pipeline.in" 
@@ -18,51 +27,79 @@ set(
   "The Jenkins pipeline Dockerfile destination name"
 )
 
-configure_file(${JENKINS_PIPELINE_DOCKERFILE_SRC} ${JENKINS_PIPELINE_DOCKERFILE_DST})
-
 set(
-  JENKINS_PIPELINE_DOCKER_SINGLE_RUN_NAME
-  "${PROJECT_BINARY_NAME_lower}-jenkins-pipeline-run"
+  JENKINS_PIPELINE_DOCKER_IMAGE_NAME
+  "${PROJECT_BINARY_NAME_lower}-jenkins-pipeline-image"
 )
 
 set(
-  JENKINS_PIPELINE_DOCKER_SINGLE_CONTAINER_NAME
+  JENKINS_PIPELINE_DOCKER_CONTAINER_NAME
   "${PROJECT_BINARY_NAME_lower}-jenkins-pipeline-run-container"
 )
 
-# --no-cache
-set(
-  JENKINS_PIPELINE_DOCKER_SINGLE_BUILD_CMD
-    DOCKER_HOST=${DOCKER_HOST_STR} DOCKER_BUILDKIT=1 ${DOCKER_EXEC} build
-      -f "${JENKINS_PIPELINE_DOCKERFILE_DST}"
-      --build-context project=${CMAKE_SOURCE_DIR} 
-      --build-arg CACHEBUST="${PROJECT_CONFIGURE_DATE}" 
-      -t ${JENKINS_PIPELINE_DOCKER_SINGLE_RUN_NAME} .
+execute_process(
+  COMMAND ${SCRIPT_EXEC} -q -c "DOCKER_HOST=${DOCKER_HOST_STR} ${DOCKER_EXEC} images -q ${JENKINS_PIPELINE_DOCKER_IMAGE_NAME}" /dev/null
+  OUTPUT_VARIABLE JENKINS_PIPELINE_DOCKER_IMAGE_NAME_PRESENT
+  ERROR_VARIABLE JENKINS_PIPELINE_DOCKER_IMAGE_NAME_PRESENT
+  OUTPUT_STRIP_TRAILING_WHITESPACE
 )
 
-#--restart=on-failure -d
-set(
-  JENKINS_PIPELINE_DOCKER_SINGLE_RUN_CMD
-    DOCKER_HOST=${DOCKER_HOST_STR} ${DOCKER_EXEC} run -it
-    -p 8080:8080 
-    -p 50000:50000
-    --name ${JENKINS_PIPELINE_DOCKER_SINGLE_CONTAINER_NAME}
-    ${JENKINS_PIPELINE_DOCKER_SINGLE_RUN_NAME}
+execute_process(
+  COMMAND ${SCRIPT_EXEC} -q -c "DOCKER_HOST=${DOCKER_HOST_STR} ${DOCKER_EXEC} ps -aq -f name=${JENKINS_PIPELINE_DOCKER_CONTAINER_NAME}" /dev/null
+  OUTPUT_VARIABLE JENKINS_PIPELINE_DOCKER_CONTAINER_PRESENT
+  ERROR_VARIABLE JENKINS_PIPELINE_DOCKER_CONTAINER_PRESENT
+  OUTPUT_STRIP_TRAILING_WHITESPACE
 )
 
-message(STATUS "docker single build command: ${DOCKER_SINGLE_BUILD_CMD}")
-message(STATUS "docker single run command: ${DOCKER_SINGLE_RUN_CMD}")
+configure_file(misc/Jenkinsfile.in Jenkinsfile @ONLY)
+
+if (JENKINS_PIPELINE_DOCKER_IMAGE_NAME_PRESENT STREQUAL "")
+  set(
+    JENKINS_PIPELINE_DOCKER_BUILD_CMD
+      DOCKER_HOST=${DOCKER_HOST_STR} DOCKER_BUILDKIT=1 ${DOCKER_EXEC} build
+        -f "${JENKINS_PIPELINE_DOCKERFILE_DST}"
+        --build-context project=${CMAKE_SOURCE_DIR} 
+        --build-arg CACHEBUST="${PROJECT_CONFIGURE_DATE}" 
+        -t ${JENKINS_PIPELINE_DOCKER_IMAGE_NAME} .
+  )
+else()
+  set(
+    JENKINS_PIPELINE_DOCKER_BUILD_CMD
+      echo "Image ${JENKINS_PIPELINE_DOCKER_IMAGE_NAME} already build"
+  )
+endif()
+
+# -d --restart=on-failure
+if (JENKINS_PIPELINE_DOCKER_CONTAINER_PRESENT STREQUAL "")
+  set(
+    JENKINS_PIPELINE_DOCKER_RUN_CMD
+      DOCKER_HOST=${DOCKER_HOST_STR} ${DOCKER_EXEC} run -it
+      -p ${JENKINS_PIPELINES_PANEL_HTTP_PORT}:${JENKINS_PIPELINES_PANEL_HTTP_PORT}
+      -p 50000:50000
+      --name ${JENKINS_PIPELINE_DOCKER_CONTAINER_NAME}
+      ${JENKINS_PIPELINE_DOCKER_IMAGE_NAME}
+  )
+else()
+  set(
+    JENKINS_PIPELINE_DOCKER_RUN_CMD
+      DOCKER_HOST=${DOCKER_HOST_STR} ${DOCKER_EXEC} container start 
+        -ia ${JENKINS_PIPELINE_DOCKER_CONTAINER_NAME}
+  )
+endif()
+
+message(STATUS "Jenkins pipeline docker build command: ${JENKINS_PIPELINE_DOCKER_BUILD_CMD}")
+message(STATUS "Jenkins pipeline docker run command: ${JENKINS_PIPELINE_DOCKER_RUN_CMD}")
 
 add_custom_target(
-  jenkins-pipeline-docker-single-build
-  COMMAND ${JENKINS_PIPELINE_DOCKER_SINGLE_BUILD_CMD}
-  COMMENT "Building the Jenkins pipeline Docker single run instance"
+  jenkins-pipeline-docker-build
+  COMMAND ${JENKINS_PIPELINE_DOCKER_BUILD_CMD}
+  COMMENT "Building the Jenkins pipeline Docker run instance"
 )
 
 add_custom_target(
-  jenkins-pipeline-docker-single-run
-  COMMAND ${JENKINS_PIPELINE_DOCKER_SINGLE_RUN_CMD}
-  DEPENDS jenkins-pipeline-docker-single-build
-  COMMENT "Running the Jenkins pipeline inside docker single run instance"
+  jenkins-pipeline-docker-run
+  COMMAND ${JENKINS_PIPELINE_DOCKER_RUN_CMD}
+  DEPENDS jenkins-pipeline-docker-build
+  COMMENT "Running the Jenkins pipeline inside docker run instance"
 )
 

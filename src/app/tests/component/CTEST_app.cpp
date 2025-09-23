e3a3f461060594bcb8aa20bcaab1527de9aae963
Yuriy Sydor <yuriysydor1991@gmail.com>
2025-09-23 13:37:48 +0300

    Introducing new app CT for MySQLController

--- src/app/tests/component/CTEST_app.cpp
+++ src/app/tests/component/CTEST_app.cpp
@@ -2,14 +2,19 @@
 #include <gtest/gtest.h>
 
 #include "src/app/ApplicationFactory.h"
+#include "src/mysqlcppconn/MySQLController.h"
 
 using namespace app;
 using namespace testing;
+using namespace mysqli;
 
 class CTEST_app : public Test
 {
  public:
   CTEST_app() = default;
+  ~CTEST_app(){
+    MySQLController::onMockCreate = nullptr;
+  }
 
   int argc{0};
   char** argv{nullptr};
@@ -65,7 +70,35 @@ TEST_F(CTEST_app, create_application_success)
 
 TEST_F(CTEST_app, execute_success)
 {
+  MockFunction<void(MySQLController&)> controllerEnsurer;
+
+  EXPECT_CALL(controllerEnsurer, Call).Times(1).WillOnce(Invoke([](MySQLController& i){
+    EXPECT_CALL(i, connect(_)).Times(1).WillOnce(Return(true));
+    EXPECT_CALL(i, get_current_date)
+            .Times(1)
+            .WillOnce(Return(std::string{"2025-09-23"}));
+  }));
+
+  MySQLController::onMockCreate = controllerEnsurer.AsStdFunction();
+
   int status = ApplicationFactory::execute(argc, argv);
 
   EXPECT_EQ(status, 0);
+}
+
+TEST_F(CTEST_app, execute_failure)
+{
+  MockFunction<void(MySQLController&)> controllerEnsurer;
+
+  EXPECT_CALL(controllerEnsurer, Call).Times(1).WillOnce(Invoke([](MySQLController& i){
+    EXPECT_CALL(i, connect(_)).Times(1).WillOnce(Return(false));
+    EXPECT_CALL(i, get_current_date)
+            .Times(0);
+  }));
+
+  MySQLController::onMockCreate = controllerEnsurer.AsStdFunction();
+
+  int status = ApplicationFactory::execute(argc, argv);
+
+  EXPECT_NE(status, 0);
 }

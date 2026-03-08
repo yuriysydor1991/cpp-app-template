fac6f919511a7431744a48d7add31c0777aba6ef
Yuriy Sydor <yuriysydor1991@gmail.com>
2026-03-08 20:53:12 +0200

    Renaming properly Nlohmann JSON library CMake enabler file

--- cmake/enablers/template-project-nlohmann-json-enabler.cmake
+++ cmake/enablers/template-project-nlohmann-json-enabler.cmake
@@ -26,7 +26,4 @@ FetchContent_Declare(
   GIT_TAG        ${TEMPLATE_APP_NLOHMANN_GIT_TAG}
 )
 
-# For Windows: Prevent overriding the parent project's compiler/linker settings
-set(NLOHMANN_force_shared_crt ON CACHE BOOL "" FORCE)
-
 FetchContent_MakeAvailable(nlohmann-json)

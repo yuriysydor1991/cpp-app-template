064ca1ccaea96e04d9f25b3f0c1afc97b0903cdf
Yuriy Sydor <yuriysydor1991@gmail.com>
2026-05-02 12:39:37 +0300

    Moving all CMake variables declarations into their respectful enablers.

--- cmake/compile-options/template-project-compile-options.cmake
+++ cmake/compile-options/template-project-compile-options.cmake
@@ -1,8 +1,19 @@
 cmake_minimum_required(VERSION 3.13)
 
+option(
+  COMPILE_WARNINGS_AS_ERRORS
+  "Set to ON if to pass -Werror compile parameter to the GCC compiler."
+  OFF
+)
+
+option(
+  ENABLE_COMPILER_CODE_ANALYZER
+  "Set to ON if to pass -fanalyzer compile parameter to the GCC compiler. May dramatically increase the compile time."
+  OFF
+)
+
 list(APPEND CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/cmake/compile-options")
 
-# CMAKE_C_COMPILER_ID STREQUAL "Clang"
 if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
   include(template-project-compile-options-MSVC)
 else()

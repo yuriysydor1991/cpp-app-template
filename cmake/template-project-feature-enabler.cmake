c9db84f60c0565808f34a4252ffcc91ab7ab80d1
Yuriy Sydor <yuriysydor1991@gmail.com>
2026-05-02 13:23:08 +0300

    Introducing sanitizers and fixing build process

--- cmake/template-project-feature-enabler.cmake
+++ cmake/template-project-feature-enabler.cmake
@@ -1,8 +1,10 @@
 cmake_minimum_required(VERSION 3.13)
 
 list(APPEND CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/cmake/compile-options")
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/cmake/compile-options/sanitizers")
 list(APPEND CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/cmake/enablers")
 list(APPEND CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/cmake/enablers/dockerers")
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/cmake/enablers/profilers")
 list(APPEND CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/cmake/enablers/packagers")
 
 include(template-project-GTest-enabler)

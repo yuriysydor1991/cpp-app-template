fffa4821b37853a995d46283d448804a9adf1978
Yuriy Sydor <yuriysydor1991@gmail.com>
2026-05-02 12:39:37 +0300

    Moving all CMake variables declarations into their respectful enablers.

--- cmake/template-project-feature-enabler-post.cmake
+++ cmake/template-project-feature-enabler-post.cmake
@@ -1,7 +1,6 @@
 cmake_minimum_required(VERSION 3.13)
 
-if (ENABLE_DOCKER)
-  include(template-project-dockerers)
-endif()
-
-include (template-project-libxml2-enabler)
+include(template-project-dockerers)
+include(template-project-nlohmann-json-enabler)
+include(template-project-libcurl-enabler)
+include(template-project-libxml2-enabler)

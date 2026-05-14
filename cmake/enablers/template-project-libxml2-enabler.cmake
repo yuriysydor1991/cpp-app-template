842b4a7976c93e3f7ce01e965da9effa271e2707
Yuriy Sydor <yuriysydor1991@gmail.com>
2026-05-14 22:46:06 +0300

    Introducing and migrating to the template_project_default_3rdparty_enabler cmake function. Making enablers git sources variables modifiable.

--- cmake/enablers/template-project-libxml2-enabler.cmake
+++ cmake/enablers/template-project-libxml2-enabler.cmake
@@ -11,32 +11,14 @@ if (NOT ENABLE_LIBXML2)
 endif()
 
 # original repo @ https://gitlab.gnome.org/GNOME/libxml2.git
-set(TEMPLATE_APP_LIBXML2_GIT "https://github.com/GNOME/libxml2.git")
-set(TEMPLATE_APP_LIBXML2_GIT_TAG "master")
+set(TEMPLATE_APP_LIBXML2_GIT "https://github.com/GNOME/libxml2.git" CACHE STRING "The LibXml2 library git source repository")
+set(TEMPLATE_APP_LIBXML2_GIT_TAG "master" CACHE STRING "The LibXml2 project git repository tag of interest")
 
-find_package(LibXml2 QUIET)
-
-if (LibXml2_FOUND)
-  message(STATUS "System already contains the LibXml2 library")
-else()
-  message(STATUS "The LibXml2 library is not available in the system")
-
-  message(STATUS "LibXml2 was not found in the system (or probing is OFF)")
-  message(STATUS "Trying to make LibXml2 library available through the Internet")
-
-  message(STATUS "LibXml2 URL: ${TEMPLATE_APP_LIBXML2_GIT}")
-  message(STATUS "LibXml2 Tag: ${TEMPLATE_APP_LIBXML2_GIT_TAG}")
-
-  include(FetchContent)
-
-  FetchContent_Declare(
-    LibXml2
-    GIT_REPOSITORY ${TEMPLATE_APP_LIBXML2_GIT}
-    GIT_TAG        ${TEMPLATE_APP_LIBXML2_GIT_TAG}
-  )
-
-  FetchContent_MakeAvailable(LibXml2)
-endif()
+template_project_default_3rdparty_enabler(
+  NAME LibXml2 
+  GIT_REPOSITORY ${TEMPLATE_APP_LIBXML2_GIT}
+  GIT_TAG ${TEMPLATE_APP_LIBXML2_GIT_TAG}
+)
 
 target_link_libraries(
   ${PROJECT_BINARY_NAME}

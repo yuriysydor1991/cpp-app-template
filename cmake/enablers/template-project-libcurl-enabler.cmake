43d322ad8d476d5854c615529ca6246a1864f12b
Yuriy Sydor <yuriysydor1991@gmail.com>
2026-05-14 22:46:06 +0300

    Introducing and migrating to the template_project_default_3rdparty_enabler cmake function. Making enablers git sources variables modifiable.

--- cmake/enablers/template-project-libcurl-enabler.cmake
+++ cmake/enablers/template-project-libcurl-enabler.cmake
@@ -10,33 +10,14 @@ if (NOT ENABLE_LIBCURL)
   return()
 endif()
 
-set(TEMPLATE_APP_CURL_GIT "https://github.com/curl/curl.git")
-set(TEMPLATE_APP_CURL_GIT_TAG "master")
+set(TEMPLATE_APP_CURL_GIT "https://github.com/curl/curl.git" CACHE STRING "The CURL library git source repository")
+set(TEMPLATE_APP_CURL_GIT_TAG "master" CACHE STRING "The CURL project git repository tag of interest")
 
-find_package(CURL QUIET)
-
-if (CURL_FOUND)
-  message(STATUS "System already contains the CURL library")
-  return()
-else()
-  message(STATUS "The CURL library is no available in the system")
-endif()
-
-message(STATUS "CURL was not found in the system (or probing is OFF)")
-message(STATUS "Trying to make CURL library available through the Internet")
-
-message(STATUS "CURL URL: ${TEMPLATE_APP_CURL_GIT}")
-message(STATUS "CURL Tag: ${TEMPLATE_APP_CURL_GIT_TAG}")
-
-include(FetchContent)
-
-FetchContent_Declare(
-  libcurl
+template_project_default_3rdparty_enabler(
+  NAME CURL 
   GIT_REPOSITORY ${TEMPLATE_APP_CURL_GIT}
-  GIT_TAG        ${TEMPLATE_APP_CURL_GIT_TAG}
+  GIT_TAG ${TEMPLATE_APP_CURL_GIT_TAG}
 )
-
-FetchContent_MakeAvailable(libcurl)
 
 target_link_libraries(
   ${PROJECT_BINARY_NAME}

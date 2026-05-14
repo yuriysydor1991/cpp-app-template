026f65b37b51254dedbf2ec64bef76ffb48cb9f9
Yuriy Sydor <yuriysydor1991@gmail.com>
2026-05-14 23:08:57 +0300

    Introducing the Qt6 enabler file to allow it's usage across multiple components of interest

---
+++ cmake/enablers/template-project-Qt6-enabler.cmake
@@ -0,0 +1,20 @@
+cmake_minimum_required(VERSION 3.13)
+
+option(
+  ENABLE_QT6 
+  "If to enable the Qt6 library (no FetchContent available)"
+  ON
+)
+
+if (NOT ENABLE_QT6)
+  return()
+endif()
+
+set(CMAKE_AUTOMOC ON)
+set(CMAKE_AUTORCC ON)
+set(CMAKE_AUTOUIC ON)
+
+find_package(
+  Qt6 REQUIRED 
+  COMPONENTS Core Gui Qml Quick QuickControls2
+)

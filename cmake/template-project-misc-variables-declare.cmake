85ea05df76575d1f9936cea630efbefccb72c15a
Yuriy Sydor <yuriysydor1991@gmail.com>
2026-05-16 13:44:34 +0300

    Introducing the snap packager integration with documentation

--- cmake/template-project-misc-variables-declare.cmake
+++ cmake/template-project-misc-variables-declare.cmake
@@ -7,6 +7,8 @@ set(
   CACHE STRING "Project main binary name and target"
 )
 
+string(TOLOWER ${PROJECT_BINARY_NAME} PROJECT_BINARY_NAME_lower)
+
 set(
   PROJECT_MAINTAINER "Your Name"
   CACHE STRING "Project maintainer name (used for the DEB package)"
@@ -21,6 +23,12 @@ set(
   MAX_LOG_LEVEL "3"
   CACHE STRING 
   "Sets the maximum severity of the compiled log messages. Error=0. Trace=5. By default Info=3"
+)
+
+set(
+  PROJECT_BINARY_INSTALLATION_DIR "bin"
+  CACHE STRING 
+  "Sets the project main binary installation directory"
 )
 
 set(

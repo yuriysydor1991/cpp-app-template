a5640493447196a9cf0d55414360c8d51666041e
Yuriy Sydor <yuriysydor1991@gmail.com>
2026-05-23 12:56:04 +0300

    Refining sdbus-c++ cmake enabler module and dependent targets

--- cmake/template-project-feature-enabler-post.cmake
+++ cmake/template-project-feature-enabler-post.cmake
@@ -5,4 +5,4 @@ include(template-project-nlohmann-json-enabler)
 include(template-project-libcurl-enabler)
 include(template-project-libxml2-enabler)
 include(template-project-OpenCV-enabler)
-include(template-project-libsdbuscxx-enabler)
+

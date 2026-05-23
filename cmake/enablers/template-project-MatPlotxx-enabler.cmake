03207cafe45d05eb339d2602d0b850f772636d9e
Yuriy Sydor <yuriysydor1991@gmail.com>
2026-05-23 15:30:18 +0300

    Implementing the Matplot++ cmake enabler module

---
+++ cmake/enablers/template-project-MatPlotxx-enabler.cmake
@@ -0,0 +1,20 @@
+cmake_minimum_required(VERSION 3.13)
+
+option(
+  ENABLE_MATPLOTCXX
+  "Set to ON to enable the MatPlotC++ (by using system wide available or through the Internet)"
+  OFF
+)
+
+if (NOT ENABLE_MATPLOTCXX)
+  return()
+endif()
+
+set(TEMPLATE_APP_MATPLOTCXX_GIT "https://github.com/alandefreitas/matplotplusplus.git" CACHE STRING "The Matplot++ library git source repository")
+set(TEMPLATE_APP_MATPLOTCXXL_GIT_TAG "master" CACHE STRING "The Matplot++ project git repository tag of interest")
+
+template_project_default_3rdparty_enabler(
+  NAME Matplot++
+  GIT_REPOSITORY ${TEMPLATE_APP_MATPLOTCXX_GIT}
+  GIT_TAG        ${TEMPLATE_APP_MATPLOTCXXL_GIT_TAG}
+)

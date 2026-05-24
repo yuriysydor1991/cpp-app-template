58e72c36a94940de7a51e58510e7f4314f08b41d
Yuriy Sydor <yuriysydor1991@gmail.com>
2026-05-24 15:05:52 +0300

    Additional refinements for the SDL3

--- cmake/enablers/template-project-SDL3-enabler.cmake
+++ cmake/enablers/template-project-SDL3-enabler.cmake
@@ -10,10 +10,8 @@ if (NOT ENABLE_SDL3)
   return()
 endif()
 
-set(TEMPLATE_APP_SDL3_GIT "https://github.com/libsdl-org/SDL.git"
-  CACHE STRING "The SDL3 library git source repository")
-set(TEMPLATE_APP_SDL3_GIT_TAG "release-3.2.10"
-  CACHE STRING "The SDL3 project git repository tag of interest")
+set(TEMPLATE_APP_SDL3_GIT "https://github.com/libsdl-org/SDL.git" CACHE STRING "The SDL3 library git source repository")
+set(TEMPLATE_APP_SDL3_GIT_TAG "release-3.2.10" CACHE STRING "The SDL3 project git repository tag of interest")
 
 find_package(OpenGL REQUIRED)
 
@@ -22,20 +20,6 @@ template_project_default_3rdparty_enabler(
   GIT_REPOSITORY ${TEMPLATE_APP_SDL3_GIT}
   GIT_TAG        ${TEMPLATE_APP_SDL3_GIT_TAG}
 )
-
-# Normalize the consumer-facing target so dependent code can always link
-# against SDL3::SDL3 regardless of whether SDL3 came in via find_package
-# (which already defines the imported target) or via FetchContent's
-# add_subdirectory (which exposes raw SDL3-shared / SDL3-static targets).
-# if(NOT TARGET SDL3::SDL3)
-#   if(TARGET SDL3-shared)
-#     add_library(SDL3::SDL3 ALIAS SDL3-shared)
-#   elseif(TARGET SDL3-static)
-#     add_library(SDL3::SDL3 ALIAS SDL3-static)
-#   elseif(TARGET SDL3)
-#     add_library(SDL3::SDL3 ALIAS SDL3)
-#   endif()
-# endif()
 
 if(NOT TARGET SDL3::SDL3)
   message(FATAL_ERROR

ca49be65af82c519d975d6ec27e04cb5bee286ca
Yuriy Sydor <yuriysydor1991@gmail.com>
2026-05-25 20:07:00 +0300

    Migrating to template_project_default_3rdparty_enabler

--- cmake/enablers/template-project-SFML-enabler.cmake
+++ cmake/enablers/template-project-SFML-enabler.cmake
@@ -13,29 +13,12 @@ endif()
 set(TEMPLATE_APP_SFML_GIT "https://github.com/skhelladi/SFML.git")
 set(TEMPLATE_APP_SFML_GIT_TAG "main")
 
-find_package(SFML COMPONENTS graphics window system QUIET)
-
-if (SFML_FOUND)
-  message(STATUS "System already contains the SFML library")
-else()
-  message(STATUS "The SFML library is not available in the system")
-
-  message(STATUS "SFML was not found in the system (or probing is OFF)")
-  message(STATUS "Trying to make SFML library available through the Internet")
-
-  message(STATUS "SFML URL: ${TEMPLATE_APP_SFML_GIT}")
-  message(STATUS "SFML Tag: ${TEMPLATE_APP_SFML_GIT_TAG}")
-
-  include(FetchContent)
-
-  FetchContent_Declare(
-    SFML
-    GIT_REPOSITORY ${TEMPLATE_APP_SFML_GIT}
-    GIT_TAG        ${TEMPLATE_APP_SFML_GIT_TAG}
-  )
-
-  FetchContent_MakeAvailable(SFML)
-endif()
+# COMPONENTS graphics window system
+template_project_default_3rdparty_enabler(
+  NAME SFML 
+  GIT_REPOSITORY ${TEMPLATE_APP_SFML_GIT}
+  GIT_TAG        ${TEMPLATE_APP_SFML_GIT_TAG}
+)
 
 target_link_libraries(
   TemplateProjectPlotGenCppControllerObj

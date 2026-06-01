b073c0175fd8123486965228090d40ef60f016bf
Yuriy Sydor <yuriysydor1991@gmail.com>
2026-06-01 11:18:15 +0300

    Simplifying root CMakeLists.txt

---
+++ cmake/template-project-bootstrap.cmake
@@ -0,0 +1,6 @@
+cmake_minimum_required(VERSION 3.13)
+
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/cmake")
+
+include(template-project-misc-variables-declare)
+include(template-project-feature-enabler)
